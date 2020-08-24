3e523a9321b8dfdd5043aff035a5621189ee03c0
jiayanfighting <jiayanfighting@163.com>
2020-08-24 15:49:59 +0800

    not ignore

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const TOKEN_KEY = 'TOKEN_KEY';
+export const PIC_KEY = 'PIC_KEY';
+
+//export const API_ROOT = 'https://139.196.8.131:8080';
+//export const ROOT = 'https://139.196.8.131:3000';
+export const API_ROOT = 'https://localhost:8080';
+export const ROOT = 'https://localhost:3000';
+
+export const JOIN_OPERATION = 'JOIN_OPERATION';
+export const NOTIFICATION_OPERATION = 'NOTIFICATION_OPERATION';
+export const FEEDBACK_OPERATION = 'FEEDBACK_OPERATION';
+export const USER_TYPE_NORMAL = 1;
+export const USER_TYPE_ADMIN = 2;
+export const USER_TYPE_SUPER_ADMIN = 3;

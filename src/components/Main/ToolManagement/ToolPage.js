8b5db91bafd513bc31704a87524576f77aae4dec
jiayanfighting <jiayanfighting@163.com>
2020-09-21 20:34:50 +0800

    多行翻译

--- src/components/Main/ToolManagement/ToolPage.js
+++ src/components/Main/ToolManagement/ToolPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Input,DatePicker,Card, Row, Col, Button,message,Form} from "antd";
+import {Input,Switch ,Card, Row, Col, Button,message,Form} from "antd";
 import 'antd/dist/antd.css';
 import ViewBoard from "../WriteManagement/ViewBoard";
 import {removeLineService} from '../../../services/toolService'
@@ -13,6 +13,7 @@ class ToolPage extends React.Component {
         translatedContent:"",
         content:"",
         copied:false,
+        showRemoveLine:false,
     };
 
     removeLine=(e)=>{
@@ -33,6 +34,12 @@ class ToolPage extends React.Component {
         });
     }
 
+    changeRemoveLine=()=>{
+        this.setState({
+            showRemoveLine:!this.state.showRemoveLine,
+        })
+    }
+
    
     render() {
         return (
@@ -49,29 +56,22 @@ class ToolPage extends React.Component {
                         </Form.Item>
                         <Form.Item >
                             <Button type="primary" htmlType="submit">转换</Button>
-                            <Button type="primary" htmlType="submit">转换并翻译</Button>
-                            <Button type="primary" htmlType="submit">直接翻译</Button>
+                            {/* <Button type="primary" htmlType="submit">转换并翻译</Button>
+                            <Button type="primary" htmlType="submit">直接翻译</Button> */}
                         </Form.Item>
                     </Form>
                     </Col>
-
-                    <Col span={8} style={{paddingLeft:5}}>
-                    <h2 style={{fontSize:"1em",fontWeight: 600,margin: 5,textAlign:"left"}}>去除换行:</h2>
-                        <div style={{padding:5}}>
-                        <TextArea style={{height:"70vh"}} value={this.state.newContent}/>
-                        {/* <ViewBoard 
-                        content={this.state.newContent}
-                        height={"70vh"}
-                        withoutTitle={true}
-                        /> */}
-                        </div>
+                    
+                    <Col span={this.state.showRemoveLine?8:16}>
                         
-                    </Col>
-                    <Col span={8}>
                         <h2 style={{fontSize:"1em",fontWeight: 600,margin: 5,textAlign:"left"}}>
-                            翻译结果:
+                            <Switch
+                                defaultUnChecked
+                                onClick={() => this.changeRemoveLine()}
+                            />&nbsp;&nbsp;
+                            翻译结果:&nbsp;&nbsp;&nbsp;
                             <CopyToClipboard text={this.state.translatedContent} onCopy={() => this.setState({copied: true})}>
-                            {this.state.copied ? <span>已复制</span> : <a>复制</a>}
+                            <a>复制</a>
                             </CopyToClipboard>
                         </h2>
                         <div style={{paddingLeft:5}}>
@@ -79,8 +79,24 @@ class ToolPage extends React.Component {
                         withoutTitle={true}
                         height={"70vh"}/>
                         </div>
-                        
                     </Col>
+
+                    {this.state.showRemoveLine?
+                        <Col span={8} style={{paddingLeft:5}}>
+                            <h2 style={{fontSize:"1em",fontWeight: 600,margin: 5,textAlign:"left"}}>去除换行:</h2>
+                            <div style={{padding:5}}>
+                            <TextArea style={{height:"70vh"}} value={this.state.newContent}/>
+                                {/* <ViewBoard 
+                                content={this.state.newContent}
+                                height={"70vh"}
+                                withoutTitle={true}
+                                /> */}
+                            </div>
+                        </Col>
+                        :
+                        ""
+                    }
+                    
                 </Row>
                
             </div>

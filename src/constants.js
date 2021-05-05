8f95c9627e9279003554820d9468006949e25d39
huangjiayan <jiayanfighting@163.com>
2021-05-05 16:57:49 +0800

    modify image root““

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ export const PIC_KEY = 'PIC_KEY';
 //export const ROOT = 'https://139.196.8.131:3000';
 export const API_ROOT = 'http://localhost:8080';
 export const ROOT = 'http://localhost:3000';
-export const IMAGE_ROOT = 'http://139.196.8.131';
+export const IMAGE_ROOT = 'http://119.29.60.226';
 // 游客账号的userId
 export const VISITOR_USERID = 2;
 
